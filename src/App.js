b19b6da5cca8fc261a4d5cf6396a1bac96d561e2
cornel97 <cornelilinca80@gmail.com>
2023-03-02 15:45:14 +0000

    lab

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import "./App.css";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 import Footer from "./Components/Footer";
+import data from "./data.json";
+import { useState } from "react";
+import SelectedBeast from "./Components/SelectedBeast";
 
 function App() {
+  const [modalShow, setModalShow] = useState(false);
+  const [theBeast, setTheBeast] = useState({});
+
+  function handleModal(beast) {
+    setModalShow(!modalShow);
+    setTheBeast(beast);
+    console.log(beast);
+  }
+
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main data={data} handleModal={handleModal} />
+      {modalShow && (
+        <SelectedBeast theBeast={theBeast} handleModal={handleModal} />
+      )}
       <Footer />
     </div>
   );
